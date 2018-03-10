b5b5ebd85ad0306ac84cca51f7d30b1d80c2f8a9
jersonlopez <jersonlopez.1204@gmail.com>
2018-03-10 11:56:08 -0500

    dojo finalizado

---
+++ app/controllers/lists.js
@@ -0,0 +1,29 @@
+import Controller from '@ember/controller';
+
+export default Controller.extend({
+   actions: {
+       create(){
+
+           var list = this.store.createRecord("list",{
+               title: this.listTitle
+            });
+            // para almacenar la informacion de la lista
+           list.save();
+       },
+
+       addItem(description,list){
+           //vamos a guardar el item
+           var item = this.store.createRecord("item",{
+               description:description
+           });
+
+           //promesa
+           list.get("items").then( ()=>{
+               list.get("items").addObject(item);
+               item.save();
+               list.save();
+
+           })
+       }
+   }
+});
